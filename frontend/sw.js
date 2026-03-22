0dc6914a49e9ebc99632c529df8ed1c0fa284293
saadegaby8-cmd <saadegaby8@gmail.com>
2026-03-21 22:50:30 -0300

    Update sw.js

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mltn-v52';
+const CACHE = 'mltn-v53';
 const STATIC = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
